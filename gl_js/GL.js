6ca7d43fff58b0febb23c58b1a0803d297383ae4
Alexey Gaev <gaevalexey@gmail.com>
2012-06-04 14:03:29 +0300

    canvas setup

--- gl_js/GL.js
+++ gl_js/GL.js
@@ -4,8 +4,9 @@ var GLace;
 	'use strict';
 
 	var instance, //for singleton instance
-		defaults: {
+		defaults = {
 			canvas: "canvas",
+			controls: "controls",
 			totalHips: 6,
 			radius: 100,
 			color: "red",
@@ -14,18 +15,36 @@ var GLace;
 		};
 
 	GLace = function (options) {
+		var controls, startX, startY, animate, canva, context;
+
 		if (instance) { //singleton pattern
 			return instance;
 		}
+		if (!(this instanceof GLace)) { //self creating constructor pattern
+			return new GLace;
+		}
 		instance = this;
 
+		options = options || {};
 		for (var key in options) {  //proper for-in pattern
 			if ( options.hasOwnProperty(key) ) {
 				defaults[key] = options[key];
 			}
 		}
 
-		this.canvas = document.getElementById(defaults.canvas);
-		this.context = this.canvas.getContext('2d');  //cache draw context
+		controls = document.getElementById(defaults.controls);
+		canva = document.getElementById(defaults.canvas);
+		context = canva.getContext('2d');  //cache draw context
+		startX = context.canvas.width / 2;
+		startY = context.canvas.height / 2;
+		//setup canvas 
+		this.canva.width = document.documentElement.clientWidth;
+		this.canva.height = document.documentElement.clientHeight - controls.offsetHeight - 5;
+		this.context.moveTo()
+
+		//private methods
+		animate = function (speed) {
+
+		}
 	}
-} ())
+} ());
