cc1d82b1ce88d18fa92ad929ae16898b3f25f44b
Alexey Gaev <gaevalexey@gmail.com>
2012-06-04 15:06:03 +0300

    Working glace on patterns (without settings and redraw for now)

--- gl_js/GL.js
+++ gl_js/GL.js
@@ -7,15 +7,35 @@ var GLace;
 		defaults = {
 			canvas: "canvas",
 			controls: "controls",
-			totalHips: 6,
-			radius: 100,
+			totalHips: 10,
+			radius: 300,
 			color: "red",
 			interval: 0,
 			speed: 0
-		};
+		}, 
+		extend;
+
+
+	//utils
+	extend = function (obj1, obj2) {
+		for (var key in obj1) {  //proper for-in pattern
+			if ( options.hasOwnProperty(key) ) {
+				obj2[key] = obj1[key];
+			}
+		}
+	}
 
 	GLace = function (options) {
-		var controls, startX, startY, animate, canva, context;
+		var controls,  //single var instruction pattern
+			startX, 
+			startY, 
+			canva, 
+			context, 
+			timers,
+			animate, 
+			showLine, 
+			clearTimers, 
+			stroke;
 
 		if (instance) { //singleton pattern
 			return instance;
@@ -26,24 +46,74 @@ var GLace;
 		instance = this;
 
 		options = options || {};
-		for (var key in options) {  //proper for-in pattern
-			if ( options.hasOwnProperty(key) ) {
-				defaults[key] = options[key];
-			}
-		}
+		extend(options, defaults);
 
+		this.iterations = 0;
+		timers = [];
 		controls = document.getElementById(defaults.controls);
 		canva = document.getElementById(defaults.canvas);
 		context = canva.getContext('2d');  //cache draw context
-		startX = context.canvas.width / 2;
-		startY = context.canvas.height / 2;
 		//setup canvas 
-		this.canva.width = document.documentElement.clientWidth;
-		this.canva.height = document.documentElement.clientHeight - controls.offsetHeight - 5;
-		this.context.moveTo()
+		canva.width = document.documentElement.clientWidth;
+		canva.height = document.documentElement.clientHeight - controls.offsetHeight - 5;
+		startX = canva.width / 2;
+		startY = canva.height / 2;
+		context.moveTo(startX, startY);
+
 
 		//private methods
-		animate = function (speed) {
+		stroke = function (color) {
+			context.strokeStyle = color;
+			context.stroke();
+		}
+		animate = function (speed, fX, fY, toX, toY) { //facade example
+			if (speed === 0) {
+				context.moveTo(fX, fY);
+				context.lineTo(toX, toY);
+			} else {
+
+			}
+		}
+		showLine = function (fX, fY, toX, toY) {
+			animate(defaults.speed, fX, fY, toX, toY);
+		}
+		clearTimers = function () {
+			if (timers.length > 0) {
+				for (var i = timers.length; i--;) {
+					clearInterval(timers[i]);
+				}
+			}
+			timers = [];
+		}
+
+		//working methods
+		this.draw = function (options) {
+			var hipsAngle,
+				totalHips,
+				hipCoords = [];
+			clearTimers();
+			options = options || {};
+			extend(options, defaults);
+			hipsAngle = (360 / defaults.totalHips) * Math.PI / 180;
+			totalHips = defaults.totalHips;
+
+			for (var currentHip = 0; currentHip < totalHips; currentHip++) {
+				var currHipCoords = {
+					x : startX + defaults.radius * Math.sin(currentHip * hipsAngle),
+					y : startY + defaults.radius * Math.cos(currentHip * hipsAngle)
+				}
+
+				for(var subHip = 0, currHipsCount = hipCoords.length; subHip < currHipsCount; subHip++) {
+					if (defaults.interval == 0) {
+						showLine(currHipCoords.x, currHipCoords.y, hipCoords[subHip].x, hipCoords[subHip].y);
+						this.iterations++;
+					}
+				}
+
+				hipCoords.push(currHipCoords);
+            }
+            
+				stroke(defaults.color);
 
 		}
 	}
